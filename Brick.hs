f4ccd534f0d5100c6e70e7b9d9bff356d6fc1508
Nathan Letwory <nathan@p3d.in>
2013-04-01 11:52:28 +0300

    Code documentation, reorganisation and renaming for clarity.

--- Brick.hs
+++ Brick.hs
@@ -1,20 +1,25 @@
 import qualified Data.Set as Set
 import qualified System.Random as R
 
+-- data type holding coordinates
 data Location = Location {x :: Int, y :: Int, z :: Int}
 	deriving (Show, Ord, Eq)
 
-
+-- data type representing a brick and its origin
 data Brick = Brick { loc :: Location }
 	deriving (Show, Ord, Eq)
 
+-- a model is a collection of bricks that are connected
 type Model = [Brick]
 
+-- helper function that gives a list of locations that
+-- are all within the first quadrant
 okQuadrant :: [Location] -> [Location]
 okQuadrant [] = error "empty list"
-okQuadrant xs = [a | a <- xs, z a > 0]
+okQuadrant xs = [a | a <- xs, z a > 0 && x a > 0 && y a > 0]
 
 -- base locations contain X and Y of brick openings
+-- from these connection points are calculated
 baseLocations :: [(Int, Int)]
 baseLocations = [
 	(-1, 1),
@@ -29,30 +34,75 @@ baseLocations = [
 	]
 
 -- create locations list
-createLocations :: Int -> Brick -> [Location]
-createLocations n brick = map (\(a, b) -> Location (a+x') (b+y') (n+z')) baseLocations 
+makeConnectionPoints :: Int -> Brick -> [Location]
+makeConnectionPoints n brick = map (\(a, b) -> Location (a+x') (b+y') (n+z')) baseLocations 
 	where
 		x' = x ( loc brick )
 		y' = y ( loc brick )
 		z' = z ( loc brick )
 
-topLocations :: Brick -> [Location]
-topLocations brick = okQuadrant $ createLocations 2 brick
-blockLocations :: Brick -> [Location]
-blockLocations brick = okQuadrant $ createLocations 0 brick
-bottomLocations :: Brick -> [Location]
-bottomLocations brick = okQuadrant $ createLocations (-2) brick
+-- Give a list of connection points on the top of a brick
+topConnectionPoints :: Brick -> [Location]
+topConnectionPoints brick = okQuadrant $ makeConnectionPoints 2 brick
+-- Give a list of connection points on the bottom of a brick
+bottomConnectionPoints :: Brick -> [Location]
+bottomConnectionPoints brick = okQuadrant $ makeConnectionPoints (-2) brick
 
+-- give a list of all connection points for a brick
+connectionPoints :: Brick -> [Location]
+connectionPoints brick = topConnectionPoints brick ++ bottomConnectionPoints brick
+
+-- Give a list of locations that cannot be occupied
+blockLocations :: Brick -> [Location]
+blockLocations brick = makeConnectionPoints 0 brick
+
+
+-- Give the list origins for a list of bricks
+origs :: [Brick] -> [Location]
+origs [] = []
+origs bs = [loc b | b <- bs]
+
+-- Give all the potential locations around given bricks
+modelLocations :: [Brick] -> [Location]
+modelLocations [] = []
+modelLocations (x:xs) = modelLocations xs ++ connectionPoints x
+
+-- Give all the blocking locations of given bricks
+modelBlockLocations :: [Brick] -> [Location]
+modelBlockLocations [] = []
+modelBlockLocations (x:xs) = modelBlockLocations xs ++ blockLocations x
+
+-- Return a random location from a list of locations
+pickConnection :: Int -> [Location] -> Location
+pickConnection _ [] = Location 1 1 1
+pickConnection seed xs = xs !! ( fst $ R.randomR(0, length xs - 1) (R.mkStdGen seed) )
+
+-- Generate a model of n bricks into list x
+model' :: (Num i, Ord i) => i -> Int -> [Brick] -> [Brick]
+model' n seed x
+	| n < 0 = []
+	| otherwise = x ++ model' (n-1) seed [brick]
+		where
+			blockS = Set.fromList $ modelBlockLocations x
+			allS = Set.fromList $ modelLocations x
+			availableL = Set.toList $ ( Set.difference allS blockS )
+			location = pickConnection seed availableL
+			brick = Brick $ location
+
+-- Generate c models of n bricks into list m of Models
+models' :: Int -> Int -> [Brick] -> [Model]
+models' c n m
+	| c <= 0 = []
+	| otherwise = [model' n c []] ++ models' (c-1) n m
+
+
+-- some test stuff
 b1 = Brick $ Location 1 1 1
 b2 = Brick $ Location 1 1 3
 b3 = Brick $ Location 1 2 1
-
-allLocations :: Brick -> [Location]
-allLocations brick = topLocations brick ++ bottomLocations brick
-
-b1all = allLocations b1
-b2all = allLocations b2
-b3all = allLocations b3
+b1all = connectionPoints b1
+b2all = connectionPoints b2
+b3all = connectionPoints b3
 
 b1set = Set.fromList ( b1all ++ b2all ++ b3all )
 --b2set = Set.fromList b2all
@@ -62,46 +112,4 @@ b1block = Set.fromList ( blockLocations b1 ++ blockLocations b2 ++ blockLocation
 --b3block = Set.fromList $ blockLocations b3
 
 totfree = length $ Set.toList ( Set.difference b1set b1block )
-
-maximum' :: (Ord a) => [a] -> a
-maximum' [] = error "empty list"
-maximum' [x] = x
-maximum' (x:xs) = max x (maximum' xs)
-
--- Give the origins of a list of bricks
-origs :: [Brick] -> [Location]
-origs [] = []
-origs bs = [loc b | b <- bs]
-
--- Give all the potential locations around given bricks
-modelLocations :: [Brick] -> [Location]
-modelLocations [] = []
-modelLocations (x:xs) = modelLocations xs ++ allLocations x
-
--- Give all the blocking locations of given bricks
-modelBlockLocations :: [Brick] -> [Location]
-modelBlockLocations [] = []
-modelBlockLocations (x:xs) = modelBlockLocations xs ++ blockLocations x
-
--- Return a random location from a list of locations
-giveLocation :: Int -> [Location] -> Location
-giveLocation _ [] = Location 1 1 1
-giveLocation r xs = xs !! ( fst $ R.randomR(0, length xs - 1) (R.mkStdGen r) )
-
--- Generate a model of n bricks, put in the list given
-model' :: (Num i, Ord i) => i -> Int -> [Brick] -> [Brick]
-model' n r x
-	| n < 0 = []
-	| otherwise = x ++ model' (n-1) r [brick]
-		where
-			blockS = Set.fromList $ modelBlockLocations x
-			allS = Set.fromList $ modelLocations x
-			availableL = Set.toList $ ( Set.difference allS blockS )
-			location = giveLocation r availableL
-			brick = Brick $ location
-
-models' :: Int -> Int -> [Brick] -> [Model]
-models' c n m
-	| c <= 0 = []
-	| otherwise = [model' n c []] ++ models' (c-1) n m
 

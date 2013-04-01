3ffebeaf148b3623c5b4d9cc581b96d2799294ff
Nathan Letwory <nathan@p3d.in>
2013-04-01 15:34:01 +0300

    Improve command-line argument handling.

--- brickudo-game.hs
+++ brickudo-game.hs
@@ -2,8 +2,16 @@ import Brick
 import System.Environment( getArgs )
 
 main = do
-	[arg] <- getArgs
-	let model_count = read arg :: Int
+	-- get command-line arguments
+	args <- getArgs
+
+ -- read model count from arguments, defaulting to 1 if empty
+	let model_count
+		| length args == 0 = 1
+		| otherwise = read ( head args ) :: Int
+
+	-- create the models
 	let models = models' model_count 10 []
+
+	-- output the models
 	print models
-	print $ length models

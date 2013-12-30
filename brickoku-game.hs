bf53d8b6afea1890902e974c7ed12e913698adf1
Nathan Letwory <nathan@letworyinteractive.com>
2013-12-30 02:41:08 +0200

    Add some information about used compiler, and available threads.
    Compile with:
    $ ghc -o brickoku-game -O -threaded -rtsopts brickoku-game.hs
    
    Then run with:
    
    ./brickoku-game +RTS -sstderr -N3
    
    Note, no use of threads yet, but interesting to see this info anyway.

--- brickoku-game.hs
+++ brickoku-game.hs
@@ -4,7 +4,18 @@ import Data.List ( intercalate )
 import System.Environment ( getArgs )
 import System.Random ( newStdGen, mkStdGen, next )
 
+import GHC.Conc
+import System.Info
+import Text.Printf
+import Data.Version
+
 main = do
+	printf "Compiled with %s-%s on %s/%s\n"
+		compilerName
+		(showVersion compilerVersion)
+		os arch
+
+	printf "Running with %d OS threads\n" numCapabilities
 	-- get command-line arguments
 	args <- getArgs
 
